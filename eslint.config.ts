2fc93b9e7d6a89e5d090a56598283f3858a0c4d5
Gaurav <gauravcoder3512@gmail.com>
2025-12-04 12:25:59 +0530

    estling

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,19 +1,10 @@
-import { defineConfig } from '@nelsonlaidev/eslint-config';
+import { defineConfig } from '@nelsonlaidev/eslint-config'
 
 export default defineConfig({
   tailwindEntryPoint: './src/app/globals.css',
-
-  overrides: [
-    {
-      files: ['**/*.{ts,tsx}'],
-      rules: {
-        // disable strict rules that break deployment
-        'better-tailwindcss/enforce-consistent-class-order': 'off',
-        'better-tailwindcss/no-unregistered-classes': [
-          'off',
-          { ignore: ['toaster', 'dark'] }
-        ]
-      }
+  overrides: {
+    tailwindcss: {
+      'better-tailwindcss/no-unregistered-classes': ['error', { ignore: ['toaster', 'dark'] }]
     }
-  ]
-});
+  }
+})
